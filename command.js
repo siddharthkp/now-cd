6c9d338f34cb28c08f453e0f18bc81b879e90a7e
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-01-12 01:28:10 +0530

    add team flag

--- command.js
+++ command.js
@@ -2,8 +2,10 @@ const pkgDir = require('pkg-dir')
 const tokens = require('./tokens')
 const rootDir = pkgDir.sync(__dirname)
 
+const teamFlag = process.argv[2] || ''
+
 const command = absolute => {
-  return `${rootDir}/node_modules/.bin/${absolute} -t ${tokens.now}`
+  return `${rootDir}/node_modules/.bin/${absolute} -t ${tokens.now} ${teamFlag}`
 }
 
 module.exports = command
