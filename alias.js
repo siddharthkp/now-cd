fbc079b023eac9ebe8ef439d6b2d942064f8d67b
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2018-05-08 16:42:39 +0530

    replace alias get

--- alias.js
+++ alias.js
@@ -1,6 +1,6 @@
-const exec = require('execa')
 const { info, loading } = require('prettycli')
-const command = require('./command')
+
+const { alias } = require('now-wrapper')
 
 const set = (deploymentURL, aliasURL) => {
   return new Promise((resolve, reject) => {
@@ -17,21 +17,15 @@ const set = (deploymentURL, aliasURL) => {
 
 const get = aliasURL => {
   return new Promise((resolve, reject) => {
-    exec
-      .shell(command(`now alias ls`))
-      .then(result => {
-        if (result.stdout) {
-          // format: url.now.sh   alias.now.sh    time
-          const aliasRow = result.stdout.split('\n').filter(u => u.includes(aliasURL))[0]
-          const url = aliasRow.split('.now.sh')[0].trim()
-          info('NOW CD', `Found previous deployment instance: ${url}`)
-          resolve(url)
-        } else {
-          info('NOW CD', 'No previous deployment instances')
-          resolve(null)
-        }
-      })
-      .catch(err => resolve(false)) // it fails when this url is not attached to a url
+    alias.get(aliasURL).then(result => {
+      if (result.url) {
+        info('NOW CD', `Found previous deployment instance: ${url}`)
+        resolve(result.url)
+      } else {
+        info('NOW CD', 'No previous deployment instances')
+        resolve(null)
+      }
+    })
   })
 }
 
